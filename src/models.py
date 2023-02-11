13ff1534d4c52777800eeded198b8182d1121587
Andres Soibelzon <a.soibelzon@gmail.com>
2023-02-11 09:28:39 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,55 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    fecha_de_suscripcion = Column(String(250), nullable=False)
+    nombre = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
+    favoritos = relationship('Favoritos', backref='usuario', lazy='true')
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Personajes(Base):
+    __tablename__ = 'personajes'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(250), nullable=False)
+    altura = Column(Integer, nullable=False)
+    genero = Column(String(250), nullable=False)
+    peso = Column(Integer, nullable=False)
+    favoritos = relationship('Favoritos', backref='personajes', lazy='true')
+
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    habitantes = Column(Integer, nullable=False)
+    periodo_orbital = Column(Integer, nullable=False)
+    diametro = Column(Integer, nullable=False)
+    favoritos = relationship('Favoritos', backref='planetas', lazy='true')
+
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250))
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    personajes_id = Column(Integer, ForeignKey('personajes.id'))
+    planetas_id = Column(Integer, ForeignKey('planetas.id'))
+
+
+    # person_id = Column(Integer, ForeignKey('person.id'))
+    # person = relationship(Person)
 
     def to_dict(self):
         return {}
